256c6426a66f720b70e0b3548c290f6e89d91aa0
esmanurbtmz <esmabitmez@hotmail.com>
2022-10-05 20:12:07 +0300

    product component edited

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-  product :any = [
+  title:string = "Ürün Listesi"
+  products :any = [
     {id:1,name:"laptop",price:5000,categoryId:1,description:"asus"},
     {id:2,name:"maouse",price:500,categoryId:2,description:"A4 tech"}
   ]
