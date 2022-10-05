0e8c58218339da6d294e83e9b32fe95903dbef15
esmanurbtmz <esmabitmez@hotmail.com>
2022-10-05 11:24:31 +0300

    project started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
