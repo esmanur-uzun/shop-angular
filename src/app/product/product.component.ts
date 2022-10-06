71055044e00b3724e5fa56fbd5761378359c4b81
esmanurbtmz <esmabitmez@hotmail.com>
2022-10-06 15:25:03 +0300

    category component edited

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -9,9 +10,9 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title:string = "Ürün Listesi"
-  products :any = [
-    {id:1,name:"laptop",price:5000,categoryId:1,description:"asus"},
-    {id:2,name:"maouse",price:500,categoryId:2,description:"A4 tech"}
+  products :Product[] = [
+    {id:1,name:"laptop",price:5000,categoryId:1,description:"asus",imageUrl:"https://images.unsplash.com/photo-1661961110372-8a7682543120?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
+    {id:2,name:"maouse",price:500,categoryId:2,description:"A4 tech",imageUrl:"https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80"}
   ]
   ngOnInit(): void {
   }
---
+++ src/app/product/product.ts
@@ -0,0 +1,10 @@
+export class Product{
+    id:number
+    name:string
+    price:number
+    categoryId: number
+    description:string
+    imageUrl:string
+    
+   
+}
