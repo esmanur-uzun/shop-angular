8f2ebf319c483fe12e45648cd7cae67d3de16706
esmanurbtmz <esmabitmez@hotmail.com>
2022-10-09 14:36:42 +0300

    filter pipe added and addToCard function writed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
+import { ProductFilterPipe } from './product/product-filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CategoryComponent,
-    ProductComponent
+    ProductComponent,
+    ProductFilterPipe,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,11 +10,16 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title:string = "Ürün Listesi"
+  filterText = ""
   products :Product[] = [
     {id:1,name:"laptop",price:5000,categoryId:1,description:"asus",imageUrl:"https://images.unsplash.com/photo-1661961110372-8a7682543120?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
     {id:2,name:"maouse",price:500,categoryId:2,description:"A4 tech",imageUrl:"https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80"}
   ]
   ngOnInit(): void {
+  }
+
+  addToCart(product:Product){
+    alert("sepete eklendi :" + product.name)
   }
 
 }
